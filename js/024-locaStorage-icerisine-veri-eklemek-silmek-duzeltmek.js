9b2becc18d35f105dbcb5df165c415b6b1ee69be
seniordoctor <recepmert034@gmail.com>
2022-08-17 15:00:10 +0300

    js

---
+++ js/024-locaStorage-icerisine-veri-eklemek-silmek-duzeltmek.js
@@ -0,0 +1,20 @@
+/*     --->  application > Local Storage kisminda gorebilirsin.
+console uzerinde
+localStorage.setItem('Recep', 'MERT');
+localStorage.getItem('Recep'); --> MERT ciktini alacagiz.
+eger tekrar setItem ('Recep', 'Code') yazarsak get ile cagirdigimizde Code ciktisini aliriz (guncelleme)
+
+silmek istersek panel uzerinden delete diyebiliriz veya;
+localStorage.removeItem('Recep') ile silebiliriz.
+
+
+
+
+
+
+
+
+
+
+
+*/
